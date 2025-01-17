567354efabd038d6040494bed0efa114cb181af9
santhosh-raja-07 <santhoshraja48z@gmail.com>
2025-01-17 08:07:46 +0530

    Updated chatpage code

--- assets/scripts/report.js
+++ assets/scripts/report.js
@@ -17,8 +17,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const closePopupButton = document.getElementById("close-popup");
   const submitButton = document.getElementById("submit-btn");
 
-  
-  // Load user data
   const userData = JSON.parse(localStorage.getItem("user")) || { userNameee: "Guest" };
   usernameDisplay.textContent = userData.userNameee;
 
